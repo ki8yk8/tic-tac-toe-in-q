c8969346b89e063e6b482cfdf957fb2654c0bb73
ki8yk8 <ki***@gmail.com>
2025-10-23 11:57:00 +0545

    adds robotic arm using the canvas

--- assets/script.js
+++ assets/script.js
@@ -36,4 +36,63 @@ class TwoPlanarArm {
 				),
 		];
 	}
+
+	draw(canvas, object) {
+		const change = {
+			theta1: object.theta1 * -1,
+			theta2: object.theta2 * -1,
+		};
+
+		const ctx = canvas.getContext("2d");
+		const [width, height] = [canvas.width, canvas.height];
+		// if change has theta 1 and theta 2 then, directly draw
+		// if change has x, y then compute inverse kinematics then draw
+
+		// drawing the base
+		ctx.fillStyle = "red";
+		ctx.fillRect((width - 100) / 2, height - 25, 100, 25);
+
+		// drawing the first link
+		const end_point = this.draw_link(
+			ctx,
+			(width - 100) / 2,
+			height - 25,
+			this.l1,
+			change.theta1
+		);
+		this.draw_link(
+			ctx,
+			end_point[0],
+			end_point[1],
+			this.l2,
+			change.theta2,
+			change.theta1,
+			"blue"
+		);
+	}
+
+	draw_link(ctx, start, end, length, theta, prev_theta = 0, color = "black") {
+		const [end_x, end_y] = [
+			start + Math.cos(theta + prev_theta) * length,
+			end + Math.sin(theta + prev_theta) * length,
+		];
+
+		ctx.beginPath();
+		ctx.moveTo(start, end);
+		ctx.lineTo(end_x, end_y);
+		ctx.strokeStyle = color;
+		ctx.lineWidth = 10;
+		ctx.lineCap = "round";
+		ctx.stroke();
+
+		return [end_x, end_y];
+	}
 }
+
+const canvas = document.getElementById("robotic-arm");
+
+const two_planer_arm = new TwoPlanarArm(70, 100);
+two_planer_arm.draw(canvas, {
+	theta1: to_radian(120),
+	theta2: to_radian(270),
+});
